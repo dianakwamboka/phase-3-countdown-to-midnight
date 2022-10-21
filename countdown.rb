4ffbf83395a043e836192f2d921b73e85f0ba3ab
Diana Kwamboka <diana.kwamboka@student.moringaschool.com>
2022-10-21 14:35:43 +0300

    DONE

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
-#write your code here
+#This lab is going to test your skills in writing while loops. a while loop will execute your block of code only while your defined conditional evaluates as true.
 
-def countdown
+
+def countdown x
+    while x >= 1 do 
+     puts "#{x} SECOND(S)!" 
+     x -=1 
+    end 
+    return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep x
+    while x >= 1 do 
+     puts "#{x} SECOND(S)!" 
+     sleep(1)
+     x -=1 
+    end 
+    return "HAPPY NEW YEAR!"
 end
